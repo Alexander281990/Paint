ed3420df85598e6f9ed3fbbddea4d7d9838cfdaa
AlexanderDell <alexivanov2890@mail.ru>
2023-01-18 10:52:37 +0300

    added lessin with jquery library

---
+++ scr_jqury.js
@@ -0,0 +1,50 @@
+//$("#some").hide(); // Прячем объект
+//let divBlock = $("#some"); // Помещаем в переменную
+//$("#some").html(); // Берем содержимое объекта
+
+let divBlock = $("#some");
+let flag = true;
+let flag_container = true;
+//divBlock.css("color", "red");
+
+$(document).ready(function() {
+    divBlock.click(function() {
+        // if (flag == true) {
+        //     $(".box_inner").animate({left: "600px"}, 1000);
+        //     divBlock.css("color", "red");
+        //     flag = false;
+        // } else {
+        //     $(".box_inner").animate({left: "50px"}, 1000);
+        //     divBlock.css("color", "black");
+        //     flag = true;
+        // }
+        $(".box_inner").animate({top: "200px"}, 500);
+        $(".box_inner").animate({top: "50px"}, 500);
+    });
+});
+
+$(document).ready(function() {
+    $(".con_head").click(function() {
+        // if(flag_container == true) {
+        //     $(".con_body").show(1000);
+        //     flag_container = false;
+        // } else {
+        //     $(".con_body").hide(1000);
+        //     flag_container = true;
+        // }
+        //$(".con_body").fadeToggle(500);
+        $(".con_body").slideToggle(1000);
+    });
+});
+
+$(document).ready(function() {
+    $(".link_1").click(function() {
+        $(".article_1").slideToggle(500);
+    });
+    $(".link_2").click(function() {
+        $(".article_2").slideToggle(500);
+    });
+    $(".link_3").click(function() {
+        $(".article_3").slideToggle(500);
+    });
+});
